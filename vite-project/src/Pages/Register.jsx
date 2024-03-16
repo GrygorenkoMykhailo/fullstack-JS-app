2bd64e8a52b2cfed1d2b969d86795846f9d38670
MykhailoGrygorenko <osnova2021@gmail.com>
2024-03-16 20:11:00 +0200

    slight improvements of register form

--- vite-project/src/Pages/Register.jsx
+++ vite-project/src/Pages/Register.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import '../css/Register.css'
 import { Form, redirect, useActionData } from "react-router-dom";
 
 function Register(){
+
+    const actionResponse = useActionData();
+    const error = useErrorMessage(actionResponse,5000);
+
     return(
         <div className="register-wrapper">
             <Form action="/register" method="POST">
@@ -15,6 +19,7 @@ function Register(){
                 <label>Confirm password: </label>
                 <input type="text" name="confirmPassword"/>
                 <button type="submit">Submit</button>
+                {error && error.error}
             </Form>
         </div>
     )
@@ -30,6 +35,10 @@ async function registerAction({request}){
             password: formData.get('password'),
         }
 
+        const repeatPassword = formData.get('confirmPassword');
+
+        if(user.password !== repeatPassword) return { error: 'Passwords are not identical'};
+
         let response = await fetch('/api/register', {
             method: 'POST',
             headers: {
@@ -37,15 +46,31 @@ async function registerAction({request}){
             },
             body: JSON.stringify(user),
         })
-        
-        let data = await response.json();
 
-        if(response.status === 302){
+        if(response.status === 302){                
+            const data = await response.json();
             return redirect('/profile/'+ data.id);
-        }else{
-            return response.status;
+        }else if(response.status === 409){
+            return { error: 'User with this email already exists' };
+        }else if(response.status === 500){
+            throw new Error();
         }    
     }
+}
+
+function useErrorMessage(errorMessage,timeout){
+    const [error,setError] = useState(null);
+
+    useEffect(() => {
+        if(errorMessage && errorMessage.error){
+            setError(errorMessage);
+            setTimeout(() => {
+                setError(null);
+            },timeout)
+        }
+    },[errorMessage])
+
+    return error;
 }
 
 export { Register,registerAction }
